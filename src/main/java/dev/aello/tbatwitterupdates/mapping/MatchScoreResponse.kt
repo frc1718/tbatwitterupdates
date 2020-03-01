ea49b1108c6fc4e9fafcf9a18a4db4d53ad0e475
Sandro Petrovski <sandro@aello.dev>
2020-03-01 18:58:08 -0500

    Switch to using MessageData

--- src/main/java/dev/aello/tbatwitterupdates/mapping/MatchScoreResponse.kt
+++ src/main/java/dev/aello/tbatwitterupdates/mapping/MatchScoreResponse.kt
@@ -1,8 +1,7 @@
 package dev.aello.tbatwitterupdates.mapping
 
-import dev.aello.tbatwitterupdates.mapping.matchScore.Match
+import dev.aello.tbatwitterupdates.mapping.matchScore.MessageData
 
 data class MatchScoreResponse(
-        val event_name: String,
-        val match: Match
+        val message_data: MessageData
 ) : Response()
